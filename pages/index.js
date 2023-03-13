9895e188bb61fc27925968c26e5aa5957b1331ae
ankuru <yuki.tori.39@gmail.com>
2023-03-13 17:14:31 +0900

    [add] nextjs test pages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Index = () => {
+  return (
+    <>
+      <div>テストページ</div>
+      <Link href="/linkSample">link Sample Page</Link>
+    </>
+  )
+}
+
+export default Index
