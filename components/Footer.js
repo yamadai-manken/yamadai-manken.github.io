a809e529cd72a9f7ba8c0a1c22f4149a951c5ecc
ankuru <yuki.tori.39@gmail.com>
2023-03-15 10:05:00 +0900

    [add] twitter
    twitterのリンクと画像をFooterに追加した

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="bg-gray-100">
-      <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center bg-black ">
+      <div className=" w-full mx-auto h-24 flex items-center justify-center bg-black ">
+	<Link href="https://twitter.com/yzmanken">
+	  <Image src="/twitter-white.svg" width={64} height={64}  alt="twitter link" />
+	</Link>
         <div className="text-white">© 2023 松に鶴</div>
       </div>
     </footer>
--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,6 @@ const Header = () => {
     <header className="sticky top-0 border-b z-10 bg-black">
       <div className="max-w-4xl mx-auto flex justify-between items-center h-12">
         <Link className="text-white" href="/">山形大学工学部漫研　松に鶴</Link>
-        <div className="text-white">Link</div>
       </div>
     </header>
   );
