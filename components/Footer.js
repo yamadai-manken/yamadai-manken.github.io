2ea1482e490fcdb3fd7a6cb77c8e4445d9ec0863
ankuru <yuki.tori.39@gmail.com>
2023-03-19 18:47:44 +0900

    [fix] remove Image
    next/Imageの最適化が静的サイトでは使用できないため

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import Link from 'next/link';
+import Link from 'next/link'
+
 const Footer = () => {
   return (
     <footer className="bg-gray-100">
       <div className=" w-full mx-auto h-24 flex items-center justify-center bg-black ">
 	<Link href="https://twitter.com/yzmanken">
-	  <Image src="/twitter-white.svg" width={44} height={44}  alt="twitter link" />
+	  <img src="/twitter-white.svg" width={44} height={44}  alt="twitter link" />
 	</Link>
 
 	<Link href="http://discord.gg/gj9aXgFTp5">
-	  <Image src="/discord-white.svg" width={44} height={44}  alt="twitter link" />
+	  <img src="/discord-white.svg" width={44} height={44}  alt="twitter link" />
 	</Link>
         <div className="text-white">© 2023 松に鶴</div>
       </div>
